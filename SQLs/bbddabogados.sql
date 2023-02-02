107180bff57dda267640dc97b6bc39cff4a23d82
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-02-02 10:10:28 +0100

    Empezado apartado 2

--- SQLs/bbddabogados.sql
+++ SQLs/bbddabogados.sql
@@ -29,7 +29,7 @@ CREATE TABLE clientela (
     CONSTRAINT pk_clientela PRIMARY KEY (codCliente),
     CONSTRAINT fk_clientela_sujetos FOREIGN KEY (codSujeto)
         REFERENCES sujetos (codSujeto)
-        ON DELETE NO ACTION ON UPDATE CASCADE
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 -- 
 CREATE TABLE abogados (
@@ -39,41 +39,50 @@ CREATE TABLE abogados (
     CONSTRAINT pk_abogados PRIMARY KEY (codAbogado),
     CONSTRAINT fk_abogados_sujetos FOREIGN KEY (codSujeto)
         REFERENCES sujetos (codSujeto)
-        ON DELETE NO ACTION ON UPDATE CASCADE
+        ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 -- 
 CREATE TABLE tipoCasos (
-    codTipoCaso INT UNSIGNED,
+    codTipoCasos INT UNSIGNED,
     desTipoCaso VARCHAR(200),
-    CONSTRAINT pk_tipoCasos PRIMARY KEY (codTipoCaso)
+    CONSTRAINT pk_tipoCasos PRIMARY KEY (codTipoCasos)
 );
 -- 
 CREATE TABLE casos (
     codCaso INT UNSIGNED,
-    codTipoCaso INT UNSIGNED,
+    codTipoCasos INT UNSIGNED,
     desCaso VARCHAR(200),
     codCliente INT UNSIGNED,
     presupuesto DECIMAL(7 , 2 ) UNSIGNED,
-    CONSTRAINT pk_casos PRIMARY KEY (codCaso , codTipoCaso),
-    CONSTRAINT fk_casos_tipoCasos FOREIGN KEY (codTipoCaso)
-        REFERENCES tipoCasos (codTipoCaso)
+    CONSTRAINT pk_casos PRIMARY KEY (codCaso , codTipoCasos),
+    CONSTRAINT fk_casos_tipoCasos FOREIGN KEY (codTipoCasos)
+        REFERENCES tipoCasos (codTipoCasos)
         ON DELETE NO ACTION ON UPDATE CASCADE,
-    CONSTRAINT fk_cassos_clientela FOREIGN KEY (codCliente)
+    CONSTRAINT fk_casos_clientela FOREIGN KEY (codCliente)
         REFERENCES clientela (codCliente)
-        ON DELETE NO ACTION ON UPDATE CASCADE
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 -- AbogadosenCasos(PK[[codcaso,codtipocaso]*,codabogado*,fecinicio], numdias)
 CREATE TABLE abogadosEnCasos (
     codCaso INT UNSIGNED,
-    codTipoCaso INT UNSIGNED,
+    codTipoCasos INT UNSIGNED,
     codAbogado INT UNSIGNED,
     fecInicio DATE,
     numDias INT(1) UNSIGNED,
-    CONSTRAINT pk_abgadosEnCasos PRIMARY KEY (codCaso , codTipoCaso , codAbogado , fecInicio),
-    CONSTRAINT fk_abogadosEnCasos_casos FOREIGN KEY (codCaso , codTipoCaso)
-        REFERENCES casos (codCaso , codTipoCaso)
+    CONSTRAINT pk_abogadosEnCasos PRIMARY KEY (codCaso , codTipoCasos , codAbogado , fecInicio),
+    CONSTRAINT fk_abogadosEnCasos_casos FOREIGN KEY (codCaso , codTipoCasos)
+        REFERENCES casos (codCaso , codTipoCasos)
         ON DELETE NO ACTION ON UPDATE CASCADE,
     CONSTRAINT fk_abogadosEnCasos_abogados FOREIGN KEY (codAbogado)
         REFERENCES abogados (codAbogado)
         ON DELETE NO ACTION ON UPDATE CASCADE
 );
+-- 
+ALTER TABLE abogadosEnCasos
+DROP FOREIGN KEY fk_abogadosEnCasos_casos,
+DROP PRIMARY KEY;
+--  
+ALTER TABLE casos
+DROP CONSTRAINT fk_casos_tipocasos,
+ADD COLUMN tipoCaso set('Civil','Penal','Laboral','Admin','Familiar') after codCaso;
+-- 
