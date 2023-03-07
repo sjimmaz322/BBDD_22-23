5b2026660f4affc00e50995b87077b2e543d3ca8
SamueljmDevs <samueljmdevs@gmail.com>
2023-03-07 16:07:12 +0100

    Terminada primera relación de ejercicios

--- SQLs/Tema6Relacion1.sql
+++ SQLs/Tema6Relacion1.sql
@@ -98,7 +98,7 @@ WHERE
 --
 --
 --
--- Ejemplo de procedure
+-- Ejemplo de procedure ejercicio 12
 delimiter $$
 drop procedure if exists buscarExtelem $$
 create procedure buscarExtelem (nombre varchar(60),apellido1 varchar(60))
@@ -113,3 +113,159 @@ end $$
 delimiter ;
 -- 
 call buscarExtelem("Juan","López");
+--
+--
+-- 11 - Prepara un procedimiento almacenado que ejecute la consulta del apartado 1 y otro que ejecute la del apartado 5.
+start transaction;
+delimiter $$
+drop procedure if exists mostrarEmpleados $$
+create procedure mostrarEmpleados()
+begin
+select * from empleados;
+end $$
+--
+drop procedure if exists mostrarEmpleadosSinComision $$
+create procedure mostrarEmpleadosSinComision()
+begin
+SELECT 
+    CONCAT(ape1em,
+            ' ',
+            IFNULL(ape2em, ''),
+            ', ',
+            nomem) AS 'Nombre completo'
+FROM
+    empleados
+WHERE
+    ifnull(comisem,0) = 0;
+end $$
+
+delimiter ;
+commit;
+--
+call mostrarEmpleados();
+call mostrarEmpleadosSinComision();
+--
+-- 13 - Prepara un procedimiento almacenado que ejecute la consulta del apartado 3 y otro para la del apartado 4 de forma que nos sirva para averiguar el nombre de aquellos que tengan el número de hijos que deseemos en cada caso.
+Start transaction;
+delimiter $$
+drop procedure if exists mostrarEmpleadosConNHijos $$
+create procedure mostrarEmpleadosConNHijos(in numHijos int)
+begin
+select  CONCAT(ape1em,
+            ' ',
+            IFNULL(ape2em, ''),
+            ', ',
+            nomem) AS 'Nombre completo'
+from empleados
+where numhiem > numHijos;
+end $$
+--
+drop procedure if exists mostrarEmpleadosConXHijos $$
+create procedure mostrarEmpleadosConXHijos(numHijosMin int, numHijosMax int)
+begin
+select  CONCAT(ape1em,
+            ' ',
+            IFNULL(ape2em, ''),
+            ', ',
+            nomem) AS 'Nombre completo'
+from empleados
+where numhiem > numHijosMin and numhiem < numHijosMax;
+end $$
+delimiter ;
+commit;
+--
+call mostarEmpleadosConNhijos(2);
+call mostrarEmpleadosConXHijos(1,3);
+--
+-- 14 - Prepara un procedimiento almacenado que, dado el nombre de un centro de trabajo, nos devuelva su dirección.
+delimiter $$
+drop procedure if exists mostrarDireccionCentro $$
+create procedure mostrarDireccionCentro(nomCentro varchar(60))
+begin
+select dirce as Dirección
+from centros
+where nomce = nomcentro;
+end $$
+delimiter ;
+--
+call mostrarDireccionCentro(' SEDE CENTRAL');
+--
+-- 15 - Prepara un procedimiento almacenado que ejecute la consulta del apartado 7 de forma que nos sirva para averiguar, dada una cantidad, el nombre de los departamentos que tienen un presupuesto superior a dicha cantidad.
+delimiter $$
+drop procedure if exists mostrarDeptosConPresupuestoSuperiorA $$
+create procedure mostrarDeptosConPresupuestoSuperiorA(presupuesto decimal(10,2))
+begin
+select nomde
+from departamentos
+where presude > presupuesto;
+end $$
+delimiter ;
+--
+call mostrarDeptosConPresupuestoSuperiorA(60000.00);
+--
+-- 16 - Prepara un procedimiento almacenado que ejecute la consulta del apartado 8 de forma que nos sirva para averiguar, dada una cantidad, el nombre de los departamentos que tienen un presupuesto igual o superior a dicha cantidad.
+delimiter $$
+drop procedure if exists mostrarDeptosConPresupuestoSuperiorOIgualA $$
+create procedure mostrarDeptosConPresupuestoSuperiorOIgualA(presupuesto decimal(10,2))
+begin
+select nomde
+from departamentos
+where presude >= presupuesto;
+end $$
+delimiter ;
+--
+call mostrarDeptosConPresupuestoSuperiorOIgualA(6000.00);
+--
+-- 17 - Prepara un procedimiento almacenado que ejecute la consulta del apartado 9 de forma que nos sirva para averiguar, dada una fecha,
+-- el nombre completo y en una sola columna de los empleados que llevan trabajando con nosotros desde esa fecha.
+delimiter $$
+drop procedure if exists mostrarEmpleadosContratadosDespuesDe $$
+create procedure mostrarEmpleadosContratadosDespuesDe (fecha Date)
+begin
+select  CONCAT(ape1em,
+            ' ',
+            IFNULL(ape2em, ''),
+            ', ',
+            nomem) AS 'Nombre completo'
+from empleados
+where fecinem > fecha;
+end$$
+delimiter ;
+--
+call mostrarEmpleadosContratadosDespuesDe('2000-1-1');
+--
+-- 18 - Prepara un procedimiento almacenado que ejecute la consulta del apartado 10 de forma que nos sirva para averiguar, dadas dos fechas,
+-- el nombre completo y en una sola columna de los empleados que comenzaron a trabajar con nosotros en el periodo de tiempo comprendido entre esas dos fechas.
+delimiter $$
+drop procedure if exists mostrarEmpleadosContratadosEntre $$
+create procedure mostrarEmpleadosContratadosEntre (fechaMin date, fechaMax date)
+begin
+select  CONCAT(ape1em,
+            ' ',
+            IFNULL(ape2em, ''),
+            ', ',
+            nomem) AS 'Nombre completo'
+from empleados
+where fecinem between fechaMin and fechaMax;
+end$$
+delimiter ;
+--
+call mostrarEmpleadosContratadosEntre('2020-1-1','2022-1-1');
+--
+-- 19 - Prepara un procedimiento almacenado que ejecute la consulta del apartado 10 de forma que nos sirva para averiguar, dadas dos fechas,
+-- el nombre completo y en una sola columna de los empleados que comenzaron a trabajar con nosotros fuera del periodo de tiempo comprendido entre esas dos fechas.
+delimiter $$
+drop procedure if exists mostrarEmpleadosContratadosFueraDe $$
+create procedure mostrarEmpleadosContratadosFueraDe (fechaMin date, fechaMax date)
+begin
+select  CONCAT(ape1em,
+            ' ',
+            IFNULL(ape2em, ''),
+            ', ',
+            nomem) AS 'Nombre completo'
+from empleados
+where fecinem not between fechaMin and fechaMax;
+end$$
+delimiter ;
+--
+call mostrarEmpleadosContratadosFueraDe('2020-1-1','2022-1-1');
