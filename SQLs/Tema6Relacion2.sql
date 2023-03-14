9da72d1e2283f46fde98af24e7c9cab579523c02
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-03-14 11:15:04 +0100

    Añadidos ejercicios

--- SQLs/Tema6Relacion2.sql
+++ SQLs/Tema6Relacion2.sql
@@ -1,1 +1,17 @@
 -- Tema 6 Relación de ejercicios 2
+/*
+Obtener todos los datos de todos los empleados y el nombre del departamento al que pertenecen.
+Obtener la extensión telefónica y el nombre del centro de trabajo de “Juan López”.
+Obtener el nombre completo y en una sola columna de los empleados del departamento “Personal” y “Finanzas”.
+Obtener el nombre del director actual del departamento “Personal”.
+Obtener el nombre de los departamentos y el presupuesto que están ubicados en la “SEDE CENTRAL”.
+Obtener el nombre de los centros de trabajo cuyo presupuesto esté entre 100000 € y 150000 €.
+Obtener las extensiones telefónicas del departamento “Finanzas”. No deben salir extensiones repetidas.
+Obtener el nombre completo y en una sola columna de todos los directores que ha tenido el departamento cualquiera.
+Como el apartado 2, pero, ahora, generalízalo para el empleado que queramos en cada caso.
+Como el apartado 3 pero generalízalo para que podamos buscar los empleados de un solo departamento.
+Como el apartado 4. pero generalízalo para buscar el director del departamento que queramos en cada caso.
+Como el apartado 5 pero generalízalo para buscar por el centro que queramos.
+Como el apartado 6 pero generalizado para poder buscar el rango que deseemos.
+Como el apartado 7 pero generalizado para poder buscar las extensiones del departamento que queramos.
+*/
