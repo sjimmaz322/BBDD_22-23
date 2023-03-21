b61935546ad25b4e5a0786babee3a281391d58a6
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-03-21 11:14:29 +0100

    Añadido ejercicio de ejemplo

--- SQLs/ejerciciosFuncionAgregado.sql
+++ SQLs/ejerciciosFuncionAgregado.sql
@@ -1,9 +1,43 @@
 use empresaclase;
 -- 
 -- Count cuenta celdas
-select count(*) as 'Número de empleados', count(distinct(empleados.numde))as 'Número departamentos'
--- select count(empleados.numem)
-from empleados;
--- 
-select sum(empleados.salarem) as 'Coste salarial'
-from empleados;
+SELECT 
+    departamentos.numde AS 'Núm Depto',
+    departamentos.nomde AS Departamento,
+    COUNT(*) AS 'Número de empleados',
+    COUNT(DISTINCT (empleados.numde)) AS 'Número departamentos',
+    SUM(empleados.salarem) AS 'Coste salarial',
+    MAX(empleados.salarem) AS 'Salario máximo',
+    MIN(empleados.salarem) AS 'Salario mínimo',
+    AVG(empleados.salarem) AS 'Salario medio'
+FROM
+    empleados
+        JOIN
+    departamentos ON empleados.numde = departamentos.numde
+GROUP BY empleados.numde;
+
+-- Rutina que devuelva el número de extensiones que utiliza un departamentos
+
+drop function if exists getNumExtensionesDepto;
+delimiter $$
+create function getNumExtensionesDepto(numDeparta int)
+returns int
+deterministic
+begin
+declare numExtensiones int;
+set numExtensiones = (SELECT 
+    
+    COUNT(DISTINCT (empleados.extelem)) AS 'Número extensiones'
+FROM
+    empleados
+        JOIN
+    departamentos ON empleados.numde = departamentos.numde
+    where departamentos.numde = numDeparta
+GROUP BY departamentos.numde);
+
+return numExtensiones;
+end $$
+
+delimiter ;
+--
+select getNumExtensionesDepto(112)as extensiones;
