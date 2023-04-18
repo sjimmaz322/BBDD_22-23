4255e62e8a052c59a2d83868d226d5318a8338ec
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-04-18 11:12:58 +0200

    Miau, no puedo con mi vida

--- SQLs/Tema7Relacion4.sql
+++ SQLs/Tema7Relacion4.sql
@@ -1,58 +1,60 @@
 -- Tema 7 relación 4
 use empresaclase;
--- 26 - Borrar de la tabla EMPLEADOS a los empleados cuyo salario (sin incluir la comisión) supere al salario medio de los empleados de su departamento.
-DROP PROCEDURE IF EXISTS ejer26;
-DELIMITER $$
-CREATE PROCEDURE ejer26(IN numDepart INT)
-BEGIN
-	DECLARE avg_salary DECIMAL(10, 2);
-	
-	SELECT AVG(salarem)
-	INTO avg_salary
-	FROM empleados
-	WHERE numde = numDepart;
-	
-	DELETE FROM empleados
-	WHERE numde = numDepart 
-	AND salarem - IFNULL(comisem, 0) > avg_salary;
-END$$
-DELIMITER ;
+/* EJER 26 */
+drop procedure if exists proc_ejer_6_4_26;
+delimiter $$
 
-CALL ejer26(122);
+create procedure proc_ejer_6_4_26()
+begin
+	-- call proc_ejer_6_4_26();
+    -- el ejercicio pide que los borremos, pero, para verlo vamos a hacer una selección:
+	/*select numem
+	from empleados 
+    where salarem >= (select avg(salarem)
+					  from empleados as e
+                      where e.numde = empleados.numde);
+	*/
+    -- HAGAMOS AHORA EL BORRADO COMO PIDE EL EJERCICIO
+    delete from empleados 
+    where salarem >= (select avg(salarem)
+					  from empleados as e
+                      where e.numde = empleados.numde);
+end $$
+delimiter ;
 
--- 27 - Disminuir en la tabla EMPLEADOS un 5% el salario de los empleados que superan el 50% del salario máximo de su departamento.
+/* EJER 27 */
+drop procedure if exists proc_ejer_6_4_27;
+delimiter $$
 
-DROP PROCEDURE IF EXISTS ejer27;
-DELIMITER $$
-CREATE PROCEDURE ejer27(IN numDepart INT)
-BEGIN
-	DECLARE max_salary DECIMAL(10, 2);
-	
-	SELECT MAX(salarem)
-	INTO max_salary
-	FROM empleados
-	WHERE numde = numDepart;
-	
-	UPDATE empleados
-    set salarem = salarem-(salarem*0.05)
-	WHERE numde = numDepart 
-	AND salarem > (max_salary/2);
-END$$
-DELIMITER ;
-
-CALL ejer27(100);
-
+create procedure proc_ejer_6_4_27()
+begin
+	-- call proc_ejer_6_4_27();
+    -- el ejercicio pide que actualicemos, pero, para verlo vamos a hacer una selección:
+	/*select numem
+	from empleados 
+    where salarem > 0.50*(select max(salarem)
+					  from empleados as e
+                      where e.numde = empleados.numde);
+	*/
+    -- HAGAMOS AHORA LA ACTUALIZACIÓN COMO PIDE EL EJERCICIO
+    update empleados 
+    set salarem = salarem*0.95
+    where salarem > 0.50*(select max(salarem)
+					  from empleados as e
+                      where e.numde = empleados.numde);
+end $$
+delimiter ;
 -- 40 - Para la base de datos de turismo rural, queremos obtener las casas disponibles para una zona y un rango de fecha dados.
-
-use gbdturrural2015;
-DROP PROCEDURE IF EXISTS ejer40;
-DELIMITER $$
-CREATE PROCEDURE ejer40(IN fecha1 date, in fecha2 date)
-BEGIN
-	select reservas.codcasa, casas.codzona
-	from reservas join casas on reservas.codcasa = casas.codcasa
-	where reservas.fecreserva not between fecha1 and fecha2;
-END$$
-DELIMITER ;
-
-CALL ejer40('2012-01-01','2012-01-31');
+drop procedure if exists ejer40;
+delimiter $$
+create procedure ejer40(fecha1 date, fecha2 date, codigoZona int)
+begin
+select casas.codcasa, casas.codzona
+from casas 
+where codcasa not in (
+			select codcasa from reservas
+            where (feciniestancia between fecha1 and fecha2)
+			or adddate(feciniestancia, interval numdiasestancia day) not between fecha1 and fecha2)
+            and codzona = codigoZona;
+end $$
+delimiter ;
