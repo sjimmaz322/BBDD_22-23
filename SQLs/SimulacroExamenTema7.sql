d709f45883440c57fdf6da8626b9ecfc2b6a8c2f
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-04-24 23:06:29 +0200

    Terminado el examen y copiado formateo de fecha como quería Eva

--- SQLs/SimulacroExamenTema7.sql
+++ SQLs/SimulacroExamenTema7.sql
@@ -31,7 +31,7 @@ create procedure ej2()
 	
     begin
     
-		select articulos.refart, articulos.desart,concat(' (',categorias.descat,')'),ventas.fecventa
+		select articulos.refart, articulos.desart,concat(' (',categorias.descat,')'),date_format(fecventa, '%M - %Y, %d (%W)')
 			from categorias 
 			join articulos on categorias.codcat = articulos.codcat
 			join detalleventa on articulos.refart = detalleventa.refart
@@ -67,3 +67,101 @@ call ej3(2012);
 /*Ejercicio 4*/
 -- Prepara una rutina que muestre un listado de artículos, su referencia, su nombre y la categoría
 -- que no hayan estado en ninguna promoción que haya empezado en este año.
+drop procedure if exists ej4;
+delimiter $$
+create procedure ej4()
+
+	begin
+		select articulos.nomart, articulos.refart, articulos.codcat
+        from articulos
+        where articulos.refart not in (
+				select catalogospromos.refart
+                from catalogospromos
+                join promociones on catalogospromos.codpromo = promociones.codpromo
+                where year(promociones.fecinipromo) = year(curdate())
+        );
+	end $$
+
+delimiter ;
+--
+call ej4();
+--
+/*Ejercicio 5*/
+-- Queremos asignar una contraseña a nuestros clientes para la APP de la cadena, prepara una rutuina que dado un dni y un teléfono,
+-- nos devueltva la contraseña inicial que estará formada por: la inicial del nombre, los números correspondientes a las posiciones 3ª, 4ª Y 5ª del dni
+-- y el número de caracteres de su nombre completo. Asegúrate que el nombre no lleva espacios a izquierda ni derecha.
+drop function if exists ej5;
+delimiter $$
+create function ej5(dni char(9),telefo char(9))
+returns varchar(6)
+deterministic
+	begin
+		declare contrasenia char(9);
+        
+        set contrasenia = (select concat(left(clientes.nomcli,1), substring(dni,3,3), length(trim(concat_ws('', clientes.nomcli, clientes.ape1cli, ifnull(clientes.ape2cli,'')))))
+							from clientes
+                            where clientes.tlfcli = telefo
+        );
+	return contrasenia;
+	end $$
+delimiter ;
+--
+select ej5('31014322H','911 703 9');
+--
+/*Ejercicio 6*/
+-- Sabemos que de nuestros vendedores almacenamos en nomvende su nombre y su primer apellido y su segundo apellido, no hay vendedores con nombres ni apellidos compuestos.
+-- Obten su contraseña formada por la inicial del nombre, las 3 primeras letras del primer apellido y las 3 primeras letras del segundo apellido.
+drop procedure if exists ej6;
+delimiter $$
+create procedure ej6(in codVendedor int)
+begin
+	select concat(left(vendedores.nomvende,1),'',substring(ape1vende,1,3),'',substring(ape2vende,1,3))
+    from vendedores
+    where vendedores.codvende = codVendedor;
+end $$
+delimiter ;
+-- 
+call ej6(1);
+--
+/*Ejercicio 7*/
+-- Queremos saber las promociones que comiencen en el mes actual y para las que la media de los precios de los artículos de dichas promociones coincidan
+-- con alguna de las de un año determinado (utiliza el ejercicio P3. Tendrás que hacer alguna modificación).
+drop procedure if exists ej7;
+delimiter $$
+CREATE PROCEDURE ej7(IN anio INT)
+BEGIN
+    SELECT promociones.despromo
+    FROM promociones 
+    JOIN catalogospromos ON promociones.codpromo = catalogospromos.codpromo
+    WHERE MONTH(promociones.fecinipromo) = MONTH(CURDATE()) 
+         GROUP BY promociones.codpromo, promociones.despromo
+    HAVING AVG(catalogospromos.precioartpromo) IN (
+        SELECT AVG(catalogospromos.precioartpromo)
+        FROM catalogospromos 
+        JOIN promociones ON catalogospromos.codpromo = promociones.codpromo
+        WHERE YEAR(promociones.fecinipromo) = anio
+        GROUP BY promociones.codpromo
+    );
+END$$
+
+delimiter ;
+--
+call ej7(2012);
+--
+/*Ejercicio 8*/
+-- Obtén un listado de artículos (referencia y nombre) cuyo precio venta sin promocionar sea el mismo que el que han tenido en alguna promoción.
+drop procedure if exists ej8;
+delimiter $$
+create procedure ej8()
+	begin
+		select articulos.refart, articulos.nomart
+        from articulos
+        where articulos.precioventa = any (
+					select catalogospromos.precioartpromo
+                    from catalogospromos join articulos on catalogospromos.refart = articulos.refart
+                    )
+        ;
+	end $$
+delimiter ;
+--
+call ej8();
