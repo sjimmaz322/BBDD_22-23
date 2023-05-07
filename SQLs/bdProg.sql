3df742545ae9b536c5443d24a890b9bcc3138fc3
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-05-07 16:09:02 +0200

    Cambiado precio de int a decimal 4,2

--- SQLs/bdProg.sql
+++ SQLs/bdProg.sql
@@ -1,8 +1,12 @@
+drop database if exists bdProg;
+--
+create database if not exists bdProg;
+--
 use bdProg;
 -- 
 CREATE TABLE productos (
     id INT,
     descripcion VARCHAR(50),
-    precio INT,
+    precio DECIMAL(4,2),
     CONSTRAINT pk_productos PRIMARY KEY (id)
 );
