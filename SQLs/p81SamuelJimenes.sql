a356579230257edaf65c09ebc0df8c94441724c7
sjimmaz322 <sjmmaz322@g.educaand.es>
2023-05-08 14:26:16 +0200

    Añadido Script para practiquilla prog

---
+++ SQLs/p81SamuelJimenes.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS p81SamuelJimenez;
+--
+CREATE DATABASE IF NOT EXISTS p81SamuelJimenez;
+--
+USE p81SamuelJimenez;
+--
+CREATE TABLE facturas(
+codigoUnico int,
+fechaEmision DATE,
+descripcion varchar(30),
+totalImporte DECIMAL(6,2),
+constraint pk_facturas PRIMARY KEY (codigoUnico)
+);
